e1b69de5011cddad2bf7e22be87cf60cb4925dc5
artemowkin <artyom.groshev2017@gmail.com>
2022-04-05 19:49:58 +0700

    Changed allowed hosts

--- kemsu_test/settings.py
+++ kemsu_test/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.getenv('DJANGO_DEBUG', '1'))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
